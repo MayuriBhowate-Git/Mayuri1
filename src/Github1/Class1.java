32807065ce2d52725c62558bfd31a5b423ddb715
mayuribhowate <mayuribhowate@MTLNGPU1LSLT025.MTL.SRV>
2024-10-18 17:39:49 +0530

    Class1 created in Github1 package.

---
+++ src/Github1/Class1.java
@@ -0,0 +1,16 @@
+package Github1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hii");
+		
+		System.out.println("Hellooo");
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mayuribhowate
+ *
+ */
+module Github {
+}
